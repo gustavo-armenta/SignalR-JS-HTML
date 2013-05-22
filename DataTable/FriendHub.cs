79b94e43370780554a181f9ef7ddaeccb95a81cc
Gustavo Armenta <gustavo_armenta@yahoo.com>
2013-05-22 13:25:37 -0700

    add await keyword

--- DataTable/FriendHub.cs
+++ DataTable/FriendHub.cs
@@ -11,29 +11,35 @@ namespace DataTable
         private static DataContext _db = new DataContext();
         private static ConcurrentDictionary<string, int> _locks = new ConcurrentDictionary<string, int>();
 
-        public override Task OnConnected()
+        public override async Task OnConnected()
         {
             var query = from f in _db.Friends
                         orderby f.Name
                         select f;
 
-            Task firstTask = Clients.Caller.all(query);
-            return firstTask.ContinueWith(task => Clients.Caller.allLocks(_locks.Values));
+            await Clients.Caller.all(query);
+            await Clients.Caller.allLocks(_locks.Values);
         }
 
-        public override Task OnReconnected()
+        public override async Task OnReconnected()
         {
             // Refresh as other users could update data while we were offline
-            return OnConnected();
+            await OnConnected();
         }
 
-        public override Task OnDisconnected()
+        public override async Task OnDisconnected()
         {
             int removed;
             _locks.TryRemove(Context.ConnectionId, out removed);
-            return Clients.All.allLocks(_locks.Values);
+            await Clients.All.allLocks(_locks.Values);
         }
 
+        public void TakeLock(Friend value)
+        {
+            _locks.AddOrUpdate(Context.ConnectionId, value.Id, (key, oldValue) => value.Id);
+            Clients.All.allLocks(_locks.Values);
+        }
+        
         public void Add(Friend value)
         {
             var added = _db.Friends.Add(value);
@@ -49,7 +55,7 @@ namespace DataTable
             _db.SaveChanges();
 
             Clients.All.delete(removed);
-        }        
+        }
 
         public void Update(Friend value)
         {
@@ -61,17 +67,6 @@ namespace DataTable
 
             int removed;
             _locks.TryRemove(Context.ConnectionId, out removed);
-            Clients.All.allLocks(_locks.Values);
-        }
-
-        public void AllLocks()
-        {
-            Clients.Caller.allLocks(_locks.Values);
-        }
-
-        public void TakeLock(Friend value)
-        {
-            _locks.AddOrUpdate(Context.ConnectionId, value.Id, (key, oldValue) => value.Id);
             Clients.All.allLocks(_locks.Values);
         }
     }
